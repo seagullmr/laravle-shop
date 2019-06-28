dc1342cfa7aa8ef260d0f537cabb8e3d8e14c7eb
seagull <seagullmr@163.com>
2019-06-28 14:56:43 +0800

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,12 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+// 未验证邮箱的用户尝试访问一个配置了 verified 中间件的路由，Laravel 就会提示该用户邮箱未激活
+Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// 启用与邮箱验证相关的路由（验证邮箱页面、重发验证邮件页面等）
+Auth::routes(['verify' => true]);
